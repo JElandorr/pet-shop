0b46bffd7b3d8e932436ea2802fb2dfe6a346b36
Jordan <jordanouzunov@abv.bg>
2024-04-26 08:54:25 +0300

    font problems solved!

---
+++ src/components/pages/Loader.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <>
+      {/* Loader */}
+      <div id="site-loader" className="load-complete">
+        <div className="loader">
+          <div className="loader-inner ball-clip-rotate">
+            <div />
+          </div>
+        </div>
+      </div>
+      {/* Loader /- */}
+    </>
+  );
+};
+
+export default Loader;
